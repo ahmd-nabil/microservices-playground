6250d038c5fa6409caca741fc1fa9a054198e60b
ahmd-nabil <ahm3dnabil99@gmail.com>
2023-12-07 17:26:32 +0200

    add movie-data-service
    Signed-off-by: ahmd-nabil <ahm3dnabil99@gmail.com>

---
+++ movie-data-service/src/test/java/nabil/microservices/moviedataservice/MovieDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nabil.microservices.moviedataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
